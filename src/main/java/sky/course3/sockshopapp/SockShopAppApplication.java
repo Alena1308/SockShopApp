ca264cfaa337c9217a03240b1ed16b1af2156689
Alena1308 <Lenamironova13@mail.ru>
2023-02-02 00:44:59 +0300

    Initial commit

---
+++ src/main/java/sky/course3/sockshopapp/SockShopAppApplication.java
@@ -0,0 +1,13 @@
+package sky.course3.sockshopapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SockShopAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SockShopAppApplication.class, args);
+    }
+
+}
